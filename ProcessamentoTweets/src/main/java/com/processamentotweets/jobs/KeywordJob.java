c9980ffd945555fbfcbebea4cba7241404390c7d
thaina <thaina@thaina-Inspiron-5566>
2017-06-23 21:36:43 -0300

    correção Job

--- ProcessamentoTweets/src/main/java/com/processamentotweets/jobs/KeywordJob.java
+++ ProcessamentoTweets/src/main/java/com/processamentotweets/jobs/KeywordJob.java
@@ -1,14 +1,11 @@
 package com.processamentotweets.jobs;
 
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.processamentotweets.api.dto.ResponseDTO;
 import com.processamentotweets.domain.Keyword;
 import com.processamentotweets.domain.KeywordRepository;
 import com.processamentotweets.domain.Tweets;
@@ -16,18 +13,8 @@ import com.processamentotweets.domain.TweetsRepository;
 import com.processamentotweets.domain.enums.StatesEnum;
 import com.processamentotweets.util.FileManager;
 
-import br.com.wexinc.boleto.app.services.BoletoService;
-import br.com.wexinc.boleto.domain.lojista.Lojista;
-import br.com.wexinc.boleto.domain.lojista.LojistaRepository;
-
 @Component
 public class KeywordJob implements InterfaceJob {
-	
-	@Autowired
-	public BoletoService boletoService;
-	
-	@Autowired
-	public LojistaRepository lojistaRepository;
 	
 	private Logger logger = Logger.getLogger(KeywordJob.class);	
 	@Autowired
@@ -41,12 +28,6 @@ public class KeywordJob implements InterfaceJob {
 	@Override
 	public void run() {
 		try {
-		List<Lojista> lojistas = lojistaRepository.findAll();
-		Calendar dataInicial = Calendar.getInstance();
-		Calendar dataFinal = Calendar.getInstance();
-		dataInicial.add(Calendar.DAY_OF_YEAR, -1);
-		List<ResponseDTO> responseList = new ArrayList<ResponseDTO>();
-		ResponseDTO responseDTO = new ResponseDTO();
 		List<Tweets> tweets = tweetsRepository.findAll();
 		for (int i = 0; i < keywords.length; i++) {
 			for (Tweets tweet : tweets) {
